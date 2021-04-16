551ae7a564f440be4e70a952f889ccf19c264d68
laiyong <laiyong@sobey.com>
2021-04-16 18:04:35 +0800

    BalanceRechargeMapper删除getTotal方法

--- sobeyMall/modules/sobeyMallPanda/src/main/java/com/sobey/module/balanceRecharge/mapper/BalanceRechargeMapper.java
+++ sobeyMall/modules/sobeyMallPanda/src/main/java/com/sobey/module/balanceRecharge/mapper/BalanceRechargeMapper.java
@@ -16,5 +16,4 @@ public interface BalanceRechargeMapper extends SupperMapper<BalanceRecharge> {
     BalanceRecharge getOne(@Param("id") Long id);
     List<BalanceRecharge> page(Page<BalanceRecharge> page,BalanceRecharge balanceRecharge);
     List<BalanceRecharge> list(BalanceRecharge balanceRecharge);
-    List getTotal();
 }
